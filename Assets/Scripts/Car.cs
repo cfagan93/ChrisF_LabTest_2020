b20c87c03e492e0016983221b68bd6db6b084f38
cfagan93 <44029422+cfagan93@users.noreply.github.com>
2020-04-21 16:56:10 +0100

    Push

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    public Transform target;
+    public List<GameObject> greenlights = new List<GameObject>();
+    public float speed;
+    public bool findnewtarget = false;
+    void Start()
+    {
+        newtarget();
+    }
+
+    void newtarget()
+    {
+        findnewtarget = false;        
+        greenlights.Clear();
+        foreach (GameObject item in GameObject.FindGameObjectsWithTag ("trafficlight"))
+        {
+            if (item.GetComponent<Lights>().colour == "green")
+            {
+                greenlights.Add(item);
+            }
+        }
+        int next = Random.Range(0, greenlights.Count);
+        target = greenlights[next].transform;
+        findnewtarget = true;
+    }
+    void Update()
+    {
+     if (target.GetComponent<Lights>().colour == "green" && target != null)
+        {
+            float move = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target.position, move);
+            transform.LookAt(target.position);
+            if (Vector3.Distance(transform.position, target.position) < 0.01f)
+            {
+                if (findnewtarget == true)
+                {
+                    newtarget();
+                }
+            }
+        }
+        else { 
+            if (findnewtarget == true)
+            {
+                newtarget();
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/Lights.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Lights : MonoBehaviour
+{
+    public Material red;
+    public Material yellow;
+    public Material green;
+    public string colour;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        int colour = Random.Range(0, 3);
+        if (colour == 0)
+        {
+           
+            StartCoroutine("ColourGreen");
+        }
+        if (colour == 1)
+        {
+
+            StartCoroutine("ColourYellow");
+        }
+        if (colour == 2)
+        {
+            StartCoroutine("ColourRed");
+        }
+    }
+
+    IEnumerator ColourRed()
+    {
+        gameObject.GetComponent<MeshRenderer>().material = red;
+        colour = "red";
+        int wait = Random.Range(5, 11);
+        yield return new WaitForSeconds(wait);
+        StartCoroutine("ColourGreen");
+    }
+    IEnumerator ColourYellow ()
+    {
+        gameObject.GetComponent<MeshRenderer>().material = yellow;
+        colour = "yellow";
+        yield return new WaitForSeconds(4);
+        StartCoroutine("ColourRed");
+    }
+    IEnumerator ColourGreen ()
+    {
+        gameObject.GetComponent<MeshRenderer>().material = green;
+        colour = "green";
+        int wait = Random.Range(5, 11);
+        yield return new WaitForSeconds(wait);
+        StartCoroutine("ColourYellow");
+    }
+    
+}
